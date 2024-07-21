462c9079285e43134160c0a3365453f6c009ff77
Znis Twayana <metwayana@gmail.com>
2024-07-21 12:04:59 +0545

    update: adds the functionality of rating the dish in dish detail page

--- src/components/rating.ts
+++ src/components/rating.ts
@@ -1,6 +1,15 @@
 export default class Rating {
-  static htmlTemplateURL = './assets/templates/components/rating.html';
-  static element = document.createElement('div');
+  static htmlTemplateURL: string = './assets/templates/components/rating.html';
+  static element: HTMLElement = document.createElement('div');
+  static starMessages: string[] = [
+    'Poor',
+    'Fair',
+    'Good',
+    'Very Good',
+    'Excellent',
+  ];
+  static numOfStars: number = 5;
+  static selectedIndex: number = -1;
   static init(id: string): HTMLElement {
     if (this.element) {
       fetch(this.htmlTemplateURL)
@@ -9,8 +18,118 @@ export default class Rating {
           this.element.classList.add('rating');
           this.element.setAttribute('id', id);
           this.element.innerHTML = html;
+          this.appendStars();
+          this.setupEventListeners();
         });
     }
     return this.element;
+  }
+
+  static appendStars(): void {
+    const starsWrapper = this.element.querySelector(
+      '.rating__stars-wrapper',
+    ) as HTMLDivElement;
+
+    for (let i = 0; i < this.numOfStars; i++) {
+      const starElement = document.createElement('i');
+      starElement.classList.add('fa-regular', 'fa-star', 'rating__star');
+      starElement.setAttribute('data-value', (i + 1).toString());
+      starsWrapper.appendChild(starElement);
+    }
+  }
+  static setupEventListeners(): void {
+    const stars = this.element.querySelectorAll('.rating__star');
+
+    stars.forEach((star, index) => {
+      star.addEventListener('mouseover', () => {
+        this.updateStars(index);
+        this.updateText(index);
+        this.updateScore(index);
+      });
+
+      star.addEventListener('mouseout', () => {
+        if (this.selectedIndex === -1) {
+          this.resetStars();
+          this.resetText();
+          this.resetScore();
+        } else {
+          this.updateStars(this.selectedIndex);
+          this.updateText(this.selectedIndex);
+          this.updateScore(this.selectedIndex);
+        }
+      });
+      star.addEventListener('click', () => {
+        this.selectedIndex = index;
+        this.persistRating(index);
+        this.updateStars(index);
+        this.updateText(index);
+        this.updateScore(index);
+      });
+    });
+    const persistedRating = this.getPersistedRating();
+    if (persistedRating !== null) {
+      this.selectedIndex = persistedRating;
+      this.updateStars(persistedRating);
+      this.updateText(persistedRating);
+      this.updateScore(persistedRating);
+    }
+  }
+
+  static updateStars(index: number): void {
+    const stars = this.element.querySelectorAll('.rating__star');
+    stars.forEach((star, i) => {
+      if (i <= index) {
+        star.classList.remove('fa-regular');
+        star.classList.add('fa-solid');
+      } else {
+        star.classList.remove('fa-solid');
+        star.classList.add('fa-regular');
+      }
+    });
+  }
+
+  static resetStars(): void {
+    const stars = this.element.querySelectorAll('.rating__star');
+    stars.forEach((star) => {
+      star.classList.remove('fa-solid');
+      star.classList.add('fa-regular');
+    });
+  }
+
+  static updateText(index: number): void {
+    const ratingText = this.element.querySelector('.rating__text');
+    if (ratingText) {
+      ratingText.textContent = this.starMessages[index];
+    }
+  }
+
+  static resetText(): void {
+    const ratingText = this.element.querySelector('.rating__text');
+    if (ratingText) {
+      ratingText.textContent = 'What do you think about this dish?';
+    }
+  }
+
+  static updateScore(index: number): void {
+    const ratingText = this.element.querySelector('.rating__score');
+    if (ratingText) {
+      ratingText.textContent = `${index + 1}/${this.numOfStars}`;
+    }
+  }
+
+  static resetScore(): void {
+    const ratingText = this.element.querySelector('.rating__score');
+    const rating = 4;
+    if (ratingText) {
+      ratingText.textContent = `${rating}/${this.numOfStars}`;
+    }
+  }
+  static persistRating(index: number): void {
+    localStorage.setItem('selectedRating', index.toString());
+  }
+
+  static getPersistedRating(): number | null {
+    const rating = localStorage.getItem('selectedRating');
+    return rating !== null ? parseInt(rating, 10) : null;
   }
 }
