1178a88255a96cc2051b01582b962ba5c53a81e1
Znis <metwayana@gmail.com>
2024-07-24 19:22:45 +0545

    update: adds CORS package

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import config from './config';
+import cors from "cors";
 import { requestLogger } from './middleware/logger';
 import { genericErrorHandler, notFoundError } from './middleware/errorHandler';
 import router from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger);
