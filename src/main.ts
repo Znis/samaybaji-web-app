2a7d9e7b5f9157614178545807f39d05112089b5
Znis Twayana <metwayana@gmail.com>
2024-07-25 07:46:50 +0545

    update: saves and loads the app state in session storage

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './style.css';
 import './router.ts';
 import AppLayout from './appLayout';
+import { StateManagement } from './state-management/stateManagement.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
+  StateManagement.loadStateFromSessionStorage();
   AppLayout.init();
 });
--- src/state-management/stateManagement.ts
+++ src/state-management/stateManagement.ts
@@ -1,22 +1,43 @@
 import Header from '../app-section/header';
-import { ICartItemData } from '../interfaces/cart';
+
 interface AppState {
   accessToken: string | null;
   user: { id: string; name: string; email: string; phoneNumber: string } | null;
-  cart: ICartItemData[];
 }
 
 export class StateManagement {
-  static state: AppState = {
-    accessToken: null,
-    user: null,
-    cart: [],
-  };
+  static state: AppState = this.loadStateFromSessionStorage();
+
   static updateState<K extends keyof AppState>(key: K, value: AppState[K]) {
     this.state[key] = value;
+    this.saveStateToSessionStorage();
     this.render();
   }
+
   static render() {
     Header.init();
+  }
+
+  static saveStateToSessionStorage() {
+    sessionStorage.setItem('appState', JSON.stringify(this.state));
+  }
+
+  static loadStateFromSessionStorage(): AppState {
+    const storedState = sessionStorage.getItem('appState');
+    if (!storedState) return this.getInitialState();
+
+    try {
+      return JSON.parse(storedState) as AppState;
+    } catch (error) {
+      console.error('Failed to parse state from sessionStorage:', error);
+      return this.getInitialState();
+    }
+  }
+
+  static getInitialState(): AppState {
+    return {
+      accessToken: null,
+      user: null,
+    };
   }
 }
